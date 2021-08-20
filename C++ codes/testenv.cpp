3c90ccc67ee720a1e59b9c40a00e6ffac32048ba
unknown <95shubham1@gmail.com>
2021-08-20 12:19:08 +0530

    test env check for dev c++

---
+++ C++ codes/testenv.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	
+	
+	vector<int>a={1,2,3,4};
+	
+	vector<int>b;
+	
+	b.push_back(1);
+	b.push_back(2);
+	
+	
+	map<int,int>mp;
+	
+	mp[1]=2;
+	mp[2]=3;
+	
+	
+	pair<int,int>aa=make_pair(1,1);
+	
+	pair<int,int>bb={1,2};
+	
+	
+	priority_queue<int,vector<int>,greater<int>>pq;
+	
+	
+	// setting working properly
+	
+	
+	
+}
